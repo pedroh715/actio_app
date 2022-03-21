d460a06b848b6c97fd86a94fbdb132130db59275
Pedro Grandizoli <pedrohenriquecontato04@gmail.com>
2022-03-21 18:20:26 -0400

    doing splash screen and button to project actio_app github 😸📱

--- app/src/main/java/com/example/kauepedro/MainActivity.java
+++ app/src/main/java/com/example/kauepedro/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -63,5 +64,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         blurLayout = findViewById(R.id.blurLayout);
+
+        MaterialCardView projectGithub = findViewById(R.id.cardGithub);
+
+        projectGithub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/pedroh715/actio_app");
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kauepedro/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.example.kauepedro;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+    VideoView videoView;
+    Handler h = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.splash_screen);
+        imageView = findViewById(R.id.image);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
+
+ /*
+        videoView = findViewById(R.id.viewVideo);
+        String path = "android.resource://com.example.kauepedro/"+R.raw.splash_video;
+
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+                imageView.setVisibility(View.GONE);
+            }
+        });
+        */
